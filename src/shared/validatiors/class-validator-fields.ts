450a516dbdcafc8ea5bc7d882861a9893b395e99
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-01-14 10:59:42 -0300

    feat: class-validator-fields

--- src/shared/validatiors/class-validator-fields.ts
+++ src/shared/validatiors/class-validator-fields.ts
@@ -2,13 +2,27 @@ import {
   FieldsErrors,
   ValidatorFieldsInterface,
 } from './validator-fields.interface';
+import { validateSync } from 'class-validator';
 
 export abstract class ClassValidatorFields<PropsValidated>
   implements ValidatorFieldsInterface<PropsValidated>
 {
   errors: FieldsErrors;
   validatedData: PropsValidated;
+
   validate(data: any): boolean {
-    throw new Error('Method not implemented.');
+    const errors = validateSync(data);
+    console.log('validateSync.errors', errors);
+    if (errors.length > 0) {
+      this.errors = {};
+      for (const error of errors) {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      }
+      return false;
+    } else {
+      this.validatedData = data;
+    }
+    return !errors.length;
   }
 }
