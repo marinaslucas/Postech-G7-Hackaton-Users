367937b310e38e7474dc6cbd0049a1b525d8d0a5
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-01-17 08:23:21 -0300

    feat: in memory unit tests

--- src/shared/domain/repositories/in-memory-repository.ts
+++ src/shared/domain/repositories/in-memory-repository.ts
@@ -17,26 +17,27 @@ export abstract class InMemoryRepository<E extends Entity>
     return this.items;
   }
   async update(newEntity: E): Promise<void> {
-    const entityToUpdate = await this._get(newEntity.id);
-    const index = this._getIndex(entityToUpdate.id);
+    const entityToUpdate = await this._get(newEntity._id);
+    const index = this._getIndex(entityToUpdate._id);
     this.items[index] = newEntity;
   }
   async delete(id: string): Promise<void> {
     const entityToDelete = await this._get(id);
-    const index = this._getIndex(entityToDelete.id);
-    this.items.slice(index, 1);
+    const index = this._getIndex(entityToDelete._id);
+    this.items.splice(index, 1);
   }
   protected async _get(id: string): Promise<E> {
     const _id = `${id}`;
-    const entity = this.items.find(item => item.id === _id);
+    const entity = this.items.find(item => item._id === _id);
     if (!entity) {
       throw new NotFoundError('Entity not found');
     }
     return entity;
   }
   protected _getIndex(id: string): number {
-    const index = this.items.findIndex(item => item.id === id);
-    if (!index) {
+    const _id = `${id}`;
+    const index = this.items.findIndex(item => item._id === _id);
+    if (index === -1) {
       throw new NotFoundError('Entity not found');
     }
     return index;
