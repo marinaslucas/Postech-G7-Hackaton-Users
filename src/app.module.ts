2a219ac968676a1be93fc8dd890ec3ce49c335d2
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-01-26 19:49:27 -0300

    fix: app configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,12 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerMiddleware } from './logger.middleware';
 import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.module';
 import { UsersModule } from './users/infraestructure/users.module';
 
 @Module({
-  imports: [EnvConfigModule.forRoot(), UsersModule], //quando criei o module pelo cli(comando), ele já importou o módulo
+  imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: '*', method: RequestMethod.ALL });
-  }
-}
+export class AppModule {}
