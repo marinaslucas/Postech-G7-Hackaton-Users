2580ddd64232697d8d6f43d8b476ae3736eb1af9
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-01-26 18:03:48 -0300

    feat: users module with dependencies

--- src/users/infraestructure/users.module.ts
+++ src/users/infraestructure/users.module.ts
@@ -4,22 +4,23 @@ import { UsersController } from './users.controller';
 import { SignupUseCase } from '../application/usecases/signup-users.usecase';
 import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository';
 import { HashProvider } from '@/shared/application/providers/implementations/hash-provider';
+import { UserRepository } from '../domain/repositories/user.repository';
 
 @Module({
   controllers: [UsersController],
   providers: [
     UsersService,
     {
-      //devo colocar todas as dependências aqui também
-      provide: 'UserRepository', //nome registrado no container
-      useClass: UserInMemoryRepository, //aqui eu coloquei o repo InMemory mas depois coloco o repo real também
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
     },
     { provide: 'HashProvider', useClass: HashProvider },
     {
-      //dentro do objeto eu passo a classe mas também digo o nome que quero, permite que eu altere o nome para o container
-      provide: SignupUseCase.UseCase, //nome para ficar no container
-      useClass: SignupUseCase.UseCase, //digo aqui a classe que vai ser usada
+      provide: SignupUseCase.UseCase,
+      useFactory: (userRepository: UserRepository.Repository, hashProvider: HashProvider) => {
+        return new SignupUseCase.UseCase(userRepository, hashProvider);
+      },
     },
   ],
 })
-export class UsersModule {}
+export class UsersModule { }
