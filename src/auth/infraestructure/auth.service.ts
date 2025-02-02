adbc8ee577d010194ff3fc80194d2eea1018616b
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-02-02 16:32:07 -0300

    feat: implementing auth service

--- src/auth/infraestructure/auth.service.ts
+++ src/auth/infraestructure/auth.service.ts
@@ -1,4 +1,30 @@
+import { EnvConfigService } from '@/shared/infraestructure/env-config/env-config.service';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+type GenerateJwtProps = {
+  accessToken: string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private envConfigService: EnvConfigService
+  ) {}
+
+  async generateJwt(userId: string): Promise<GenerateJwtProps> {
+    const accessToken = await this.jwtService.signAsync({
+      payload: { id: userId },
+      options: {},
+    });
+    return { accessToken };
+  }
+
+  async verifyJwt(token: string) {
+    const payload = await this.jwtService.verifyAsync(token, {
+      secret: this.envConfigService.getJwtSecret(),
+    });
+    return payload;
+  }
+}
