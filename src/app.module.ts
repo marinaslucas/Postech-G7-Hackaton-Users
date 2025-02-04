7aeded86fb86b81da232cd7e15924d7f35d20426
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-02-03 23:24:50 -0300

    video resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { EnvConfigModule } from './shared/infraestructure/env-config/env-config.
 import { UsersModule } from './users/infraestructure/users.module';
 import { DatabaseModule } from './shared/infraestructure/database/database.module';
 import { AuthModule } from './auth/infraestructure/auth.module';
+import { VideoModule } from './video/infraestructure/video.module';
 
 @Module({
-  imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule],
+  imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule, VideoModule],
 })
 export class AppModule {}
