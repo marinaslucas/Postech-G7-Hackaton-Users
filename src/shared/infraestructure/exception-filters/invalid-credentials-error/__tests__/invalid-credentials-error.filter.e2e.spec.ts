8b3af9a5d455532e1485d92dfe47f96e1d41b24e
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-02-03 22:22:37 -0300

    refactor: formatting files

--- src/shared/infraestructure/exception-filters/invalid-credentials-error/__tests__/invalid-credentials-error.filter.e2e.spec.ts
+++ src/shared/infraestructure/exception-filters/invalid-credentials-error/__tests__/invalid-credentials-error.filter.e2e.spec.ts
@@ -8,32 +8,32 @@ import { InvalidCredentialsError } from 'src/shared/application/errors/Invalid-c
 class StubController {
   @Get()
   index() {
-    throw new InvalidCredentialsError('Invalid credentials')
+    throw new InvalidCredentialsError('Invalid credentials');
   }
 }
 
 describe('InvalidCredentialsErrorFilter e2e tests', () => {
-  let app: INestApplication
-  let module: TestingModule
+  let app: INestApplication;
+  let module: TestingModule;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
       controllers: [StubController],
-    }).compile()
-    app = module.createNestApplication()
-    app.useGlobalFilters(new InvalidCredentialsErrorFilter())
-    await app.init()
-  })
+    }).compile();
+    app = module.createNestApplication();
+    app.useGlobalFilters(new InvalidCredentialsErrorFilter());
+    await app.init();
+  });
 
   it('should be defined', () => {
-    expect(new InvalidCredentialsErrorFilter()).toBeDefined()
-  })
+    expect(new InvalidCredentialsErrorFilter()).toBeDefined();
+  });
 
   it('should catch a InvalidCredentialsError', () => {
     return request(app.getHttpServer()).get('/stub').expect(400).expect({
       statusCode: 400,
       error: 'Bad Request',
       message: 'Invalid credentials',
-    })
-  })
-})
+    });
+  });
+});
