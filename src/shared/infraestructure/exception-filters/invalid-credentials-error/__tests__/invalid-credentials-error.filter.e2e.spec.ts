5d347614eb40832956966fd65ef97e782f53d071
Marina Lucas <71855361+marinaslucas@users.noreply.github.com>
2025-02-03 22:18:55 -0300

    fix: tests

--- src/shared/infraestructure/exception-filters/invalid-credentials-error/__tests__/invalid-credentials-error.filter.e2e.spec.ts
+++ src/shared/infraestructure/exception-filters/invalid-credentials-error/__tests__/invalid-credentials-error.filter.e2e.spec.ts
@@ -2,40 +2,38 @@ import { Controller, Get, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 import { InvalidCredentialsErrorFilter } from '../invalid-credentials-error.filter';
-
-class InvalidCredentialsError extends Error {
-  constructor(public message: string) {
-    super(message);
-    this.name = 'InvalidCredentialsError';
-  }
-}
+import { InvalidCredentialsError } from 'src/shared/application/errors/Invalid-credentials-error';
 
 @Controller('stub')
 class StubController {
   @Get()
   index() {
-    throw new InvalidCredentialsError('Invalid credentials');
+    throw new InvalidCredentialsError('Invalid credentials')
   }
 }
+
 describe('InvalidCredentialsErrorFilter e2e tests', () => {
-  let app: INestApplication;
-  let module: TestingModule;
+  let app: INestApplication
+  let module: TestingModule
+
   beforeAll(async () => {
     module = await Test.createTestingModule({
       controllers: [StubController],
-    }).compile();
-    app = module.createNestApplication();
-    app.useGlobalFilters(new InvalidCredentialsErrorFilter());
-    await app.init();
-  });
+    }).compile()
+    app = module.createNestApplication()
+    app.useGlobalFilters(new InvalidCredentialsErrorFilter())
+    await app.init()
+  })
+
   it('should be defined', () => {
-    expect(new InvalidCredentialsErrorFilter()).toBeDefined();
-  });
+    expect(new InvalidCredentialsErrorFilter()).toBeDefined()
+  })
+
   it('should catch a InvalidCredentialsError', () => {
     return request(app.getHttpServer()).get('/stub').expect(400).expect({
       statusCode: 400,
       error: 'Bad Request',
       message: 'Invalid credentials',
-    });
-  });
-});
+    })
+  })
+})
