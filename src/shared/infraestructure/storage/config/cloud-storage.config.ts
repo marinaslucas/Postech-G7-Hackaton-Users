8066822ec9cafaf11ee8352a0dad3f419bdf034d
@F0129560 <luanssrr@hotmail.com>
2025-02-06 21:32:13 -0300

    feat: ajuste para funcionar o storage local ou deployado

--- src/shared/infraestructure/storage/config/cloud-storage.config.ts
+++ src/shared/infraestructure/storage/config/cloud-storage.config.ts
@@ -2,10 +2,16 @@ import { Storage } from '@google-cloud/storage';
 
 const BUCKET_NAME = process.env.GCLOUD_STORAGE_BUCKET;
 
-const storage = new Storage({
-  projectId: process.env.GCLOUD_PROJECT_ID,
-  keyFilename: process.env.GCLOUD_KEY_FILENAME,
-});
+const storage = process.env.NODE_ENV === "test" || !process.env.NODE_ENV
+  ? new Storage({
+      projectId: process.env.GCLOUD_PROJECT_ID,
+      keyFilename: process.env.GCLOUD_KEY_FILENAME,
+    })
+  : new Storage({
+      projectId: process.env.GCLOUD_PROJECT_ID,
+      credentials: JSON.parse(process.env.GCP_SERVICE_ACCOUNT_KEY), // JSON direto da secret
+    });
+
 
 export const cloudStorage = {
   storage,
